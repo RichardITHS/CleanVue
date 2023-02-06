900f78e1e4dee8705adfd216ec85fd6e365decdf
Navigoo <81353827+Navigoo@users.noreply.github.com>
2023-02-06 12:01:19 +0100

    Second commit

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,11 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+/* Efter nom install måste ni importera biblioteket här... */
 
 import "./assets/main.css";
 
 const app = createApp(App);
 
+/* Biblioteket används sedan med use  */
 app.use(router);
 
 app.mount("#app");
